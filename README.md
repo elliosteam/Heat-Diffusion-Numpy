dd30a7c3580b4abaf2a3649388e2e2e146cc18d1
Ell <elliosteam@gmail.com>
2025-11-04 11:35:36 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 1D Heat Diffusion Simulation
+A simple numerical simulation of the 1-D heat equation using finite-difference methods in NumPy.
+
+**Equation:**
+∂T/∂t = α ∂²T/∂x²
+
+**Files:**
+- `plot.py`: core simulation.
+- `requirements.txt`: dependencies.
+
+Run:
+python plot.py
---
+++ plot.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+n = 50 
+
+temp = np.zeros(n)
+temp[n//2] = 100
+
+
+for _ in range(200):
+    temp[1:-1] += 0.1 * (temp[:-2] - 2*temp[1:-1] + temp[2:])
+
+plt.style.use("ggplot")
+plt.plot(temp)
+plt.title("Heat shit")
+plt.show()
